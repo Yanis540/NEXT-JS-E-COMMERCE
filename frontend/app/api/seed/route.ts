4ea722add02be6d7aec5022da41b29a30da4fcf1
Yanis540 <yanistabellout4@gmail.com>
2023-06-20 15:39:31 +0100

    working out

--- frontend/app/api/seed/route.ts
+++ frontend/app/api/seed/route.ts
@@ -1,46 +1,33 @@
-import { NextResponse } from "next/server";
-import fs from 'fs'
-import path from 'path';
-import { db } from "@/libs/db";
-const splitStringByNotQuotedSemicolon = (input: string): string[] => {
-    const result = [];
+// import { NextResponse } from "next/server";
+// import fs from 'fs'
+// import path from 'path';
+// import { db } from "@/libs/db";
+// const splitStringByNotQuotedSemicolon = (input: string): string[] => {
+//     const result = [];
+
+import { NextResponse } from "next/server"
+
   
-    let currentSplitIndex = 0;
-    let isInString = false;
-    for (let i = 0; i < input.length; i++) {
-      if (input[i] === "'") {
-        // toggle isInString
-        isInString = !isInString;
-      }
-      if (input[i] === ';' && !isInString) {
-        result.push(input.substring(currentSplitIndex, i + 1));
-        currentSplitIndex = i + 2;
-      }
-    }
+//     let currentSplitIndex = 0;
+//     let isInString = false;
+//     for (let i = 0; i < input.length; i++) {
+//       if (input[i] === "'") {
+//         // toggle isInString
+//         isInString = !isInString;
+//       }
+//       if (input[i] === ';' && !isInString) {
+//         result.push(input.substring(currentSplitIndex, i + 1));
+//         currentSplitIndex = i + 2;
+//       }
+//     }
   
-    return result;
-}
+//     return result;
+// }
 
 
 export async function POST(req:Request){
     try{
-        const rawSql = await fs.promises.readFile(path.join(__dirname, 'ecommerce.sql'), {
-            encoding: 'utf-8',
-        });
-        console.log(rawSql)
-        const sqlReducedToStatements = rawSql
-        .split('\n')
-        .filter((line) => !line.startsWith('--')) // remove comments-only lines
-        .join('\n')
-        .replace(/\r\n|\n|\r/g, ' ') // remove newlines
-        .replace(/\s+/g, ' '); // excess white space
-        const sqlStatements = splitStringByNotQuotedSemicolon(sqlReducedToStatements);
-    
-        for (const sql of sqlStatements) {
-            console.log('SQL' , sql)
-            await db.$executeRawUnsafe(sql);
-        }
-
+        
         return NextResponse.json({})
     }
     catch(err:any){
@@ -49,3 +36,30 @@ export async function POST(req:Request){
         return new NextResponse("Internal Server Error",{status:500})
     }
 }
+// export async function POST(req:Request){
+//     try{
+//         const rawSql = await fs.promises.readFile(path.join(__dirname, 'ecommerce.sql'), {
+//             encoding: 'utf-8',
+//         });
+//         console.log(rawSql)
+//         const sqlReducedToStatements = rawSql
+//         .split('\n')
+//         .filter((line) => !line.startsWith('--')) // remove comments-only lines
+//         .join('\n')
+//         .replace(/\r\n|\n|\r/g, ' ') // remove newlines
+//         .replace(/\s+/g, ' '); // excess white space
+//         const sqlStatements = splitStringByNotQuotedSemicolon(sqlReducedToStatements);
+    
+//         for (const sql of sqlStatements) {
+//             console.log('SQL' , sql)
+//             await db.$executeRawUnsafe(sql);
+//         }
+
+//         return NextResponse.json({})
+//     }
+//     catch(err:any){
+
+//         console.log(err.message,"ERROR_SEED")
+//         return new NextResponse("Internal Server Error",{status:500})
+//     }
+// }
